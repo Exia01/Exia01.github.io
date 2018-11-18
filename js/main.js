3e230ddfad15640562feec75a6d6d2fd67c5f2cc
Jose <sixgg4@gmail.com>
2018-11-18 15:29:40 -0500

    functionality changes
    -Scrollspy action to skills in sidenav
    -Modal witdth increase from 65% to 90% in mobile
    -added contact mailto-links
    -resume links
    
    Missing:
    *Scroll to top arrow
    *minor aesthetic changes

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    "use strict"; // Start of use strict
+    /* Initializing variables */
+
+    $('.sidenav').sidenav();
+    /* materialbox pops up the image */
+    $('.materialboxed').materialbox();
+    /* Enables modal pop-up */
+    $('.modal').modal();
+
+    /* Initializes the hover */
+    $('.tooltipped').tooltip();
+
+    $('.scrollspy').scrollSpy()
+
+
+    // collapsible skills menu
+    $(document).ready(function () {
+        $('.collapsible').collapsible();
+    });
+
+    // change text using ternary operator 
+    /* See Reference here:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator */
+    $("#parent-text").click(function () {
+        $("#child-text").fadeOut(function () {
+            $("#child-text").text(($("#child-text").text() == 'add') ? 'clear' : 'add').fadeIn();
+        })
+    })
+
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            let target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 70)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+    
+      // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function () {
+        $('.sidenav').sidenav('close');
+    });
+
+
+
+});
